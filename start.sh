c71bd2b42aa23298305901bb6e85268ee6874ec1
fff4rfded <95513800+fff4rfded@users.noreply.github.com>
2022-06-14 08:42:09 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=0b73caa7-8ca0-4492-9aa6-f79402f0c8bf
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/fff4rfded/hcta/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/fff4rfded/hcta/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/fff4rfded/hcta/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
